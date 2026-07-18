ab10c8c8f80f3b28638f2fbf631de9ed325cf79e
Aydaan <13565@holbertonstudents.com>
2026-07-18 14:54:04 +0400

    fixed

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -7,7 +7,7 @@ import csv
 from typing import List
 def index_range(page: int, page_size: int) -> tuple:
     """
-    Return start and end indexes for pagination.
+    Return a tuple containing the start and end indexes for pagination.
     """
     return ((page - 1) * page_size, page * page_size)
 class Server:
@@ -16,9 +16,7 @@ class Server:
     """
     DATA_FILE = "Popular_Baby_Names.csv"
     def __init__(self):
-        """
-        Initialize the server.
-        """
+        """Initialize the server."""
         self.__dataset = None
     def dataset(self) -> List[List]:
         """
@@ -30,10 +28,9 @@ class Server:
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
         return self.__dataset
-    def get_page(self, page: int = 1,
-                 page_size: int = 10) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        Return a page of the dataset.
+        Return the appropriate page of the dataset.
         """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
@@ -41,6 +38,5 @@ class Server:
         start, end = index_range(page, page_size)
         if start >= len(data):
             return []
-
         return data[start:end]
     
