2a604b131107aab69bb3accd28c0b33e1867cbae
Aydaan <13565@holbertonstudents.com>
2026-07-18 15:10:17 +0400

    fixed

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,20 +1,18 @@
-#!/usr/bin/env python3
-import csv
-import math
-from typing import List
 def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
     """
-    Return hypermedia pagination information
+    Return hypermedia pagination information.
     """
-data = self.get_page(page, page_size)
-total_pages = math.ceil(len(self.dataset()) / page_size)
-prev_page = page - 1 if page > 1 else None
-next_page = page + 1 if page < total_pages else None
-return {
-    "page_size": len(data),
-    "page": page,
-    "data": data,
-    "next_page": next_page,
-    "prev_page": prev_page,
-    "total_pages": total_pages,
-}
+    data = self.get_page(page, page_size)
+    total_pages = math.ceil(len(self.dataset()) / page_size)
+
+    prev_page = page - 1 if page > 1 else None
+    next_page = page + 1 if page < total_pages else None
+
+    return {
+        "page_size": len(data),
+        "page": page,
+        "data": data,
+        "next_page": next_page,
+        "prev_page": prev_page,
+        "total_pages": total_pages,
+    }
