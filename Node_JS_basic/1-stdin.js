3238c1db30a0d5f1c9d70df55940e724a659041e
Aydaan <13565@holbertonstudents.com>
2026-08-09 06:27:33 +0400

    Use process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,11 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const input = process.stdin.read();
+process.stdin.setEncoding('utf8');
 
-  if (input !== null) {
-    console.log(`Your name is: ${input.toString().trim()}`);
-  }
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
