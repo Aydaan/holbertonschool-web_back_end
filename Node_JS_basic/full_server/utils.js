a3516f321d82d97c55e09ba95e17f6fe0290aaca
Aydaan <13565@holbertonstudents.com>
2026-08-09 07:45:54 +0400

    Fix full server

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-export function readDatabase(filePath) {
+function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf-8', (err, data) => {
       if (err) {
@@ -9,7 +9,7 @@ export function readDatabase(filePath) {
       }
 
       const lines = data.split('\n');
-      const fields = {};
+      const students = {};
 
       for (let i = 1; i < lines.length; i += 1) {
         if (!lines[i].trim()) {
@@ -18,14 +18,16 @@ export function readDatabase(filePath) {
 
         const [firstname, lastname, age, field] = lines[i].split(',');
 
-        if (!fields[field]) {
-          fields[field] = [];
+        if (!students[field]) {
+          students[field] = [];
         }
 
-        fields[field].push(firstname);
+        students[field].push(firstname);
       }
 
-      resolve(fields);
+      resolve(students);
     });
   });
 }
+
+export { readDatabase };
