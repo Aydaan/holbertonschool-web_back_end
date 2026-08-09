d14343e144ddac18eec389a08176c54635ab207a
Aydaan <13565@holbertonstudents.com>
2026-08-09 08:21:46 +0400

    Fix response status handling

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -16,10 +16,10 @@ class StudentsController {
           students.push(msg);
         }
 
-        response.send(200, `${students.join('\n')}`);
+        response.status(200).send(students.join('\n'));
       })
       .catch(() => {
-        response.send(500, 'Cannot load the database');
+        response.status(500).send('Cannot load the database');
       });
   }
 
@@ -27,15 +27,15 @@ class StudentsController {
     const { major } = request.params;
 
     if (major !== 'CS' && major !== 'SWE') {
-      response.send(500, 'Major parameter must be CS or SWE');
+      response.status(500).send('Major parameter must be CS or SWE');
     } else {
       readDatabase(DATABASE)
         .then((fields) => {
           const students = fields[major];
 
-          response.send(200, `List: ${students.join(', ')}`);
+          response.status(200).send(`List: ${students.join(', ')}`);
         })
-        .catch(() => response.send(500, 'Cannot load the database'));
+        .catch(() => response.status(500).send('Cannot load the database'));
     }
   }
 }
